870c4b6f1451b270ae08d028f462a4fdf25141b1
Lorenz Sieben <me@lorenz-sieben.com>
2018-11-05 01:07:40 +0200

    Resolve paths correctly for icons

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
-module.exports = {
+const path = require('path');
+
+module.exports = ctx => {return {
     plugins: [
         require('postcss-fonticons')({
-            iconPath: 'assets/icons/',
+            iconPath: path.resolve(__dirname, 'assets/icons/'),
         }),
         require('postcss-preset-env')({
             browsers: [ 'last 2 versions', '> 5%' ],
         }),
         require('cssnano')({}),
     ]
-};
+}};
